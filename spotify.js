017ec0efa763cf5cf0a9cfd484428714e15fa827
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-04-27 19:24:08 -0400

    Show albums for each artist

--- spotify.js
+++ spotify.js
@@ -1,4 +1,5 @@
 $(document).on("ready", function(){
+	$(".js-album-heading").hide();
 
 	$(".js-submit").on("submit", function(event){
 		event.preventDefault();
@@ -29,13 +30,45 @@ function displayInfo(the_array){
 				<li>
 					<p>Name: ${the_item.name}</p>
 					<img src="${the_item.images[0].url}" width="350px">
+					<button class="btn btn-primary js-album-btn" data-artist-id="${the_item.id}">Search Albums</button>
 				</li>`;
 		} else { 
 			var html = `
 				<li>
 					<p>Name: ${the_item.name}</p>
+					<button class="btn btn-primary js-album-btn" data-artist-id="${the_item.id}">Search Albums</button>
 				</li>`
 		}
 		$(".js-info").append(html);
 	});
+
+	$(".js-info").on("click", ".js-album-btn", function(event){
+			var btn = event.currentTarget;
+			var id = $(btn).data("artist-id");
+
+				$.ajax({
+				url: `https://api.spotify.com/v1/artists/${id}/albums`,
+				data: id,
+				success: function(Data){
+					console.log("It worked!");
+					console.log(Data.items[0].name);
+					displayAlbums(Data.items)
+				},
+				error: function(theError){
+					console.log("It failed.");
+					console.log(theError.responseJSON);
+				}
+			});
+	});
+};
+
+function displayAlbums(the_array){
+	the_array.forEach(function(the_item){
+		var htmlAlbum = `
+			<li>
+				<p>Albums: ${the_item.name}</p>
+			</li>`
+	$(".js-album-heading").show();
+	$(".js-album").append(htmlAlbum);
+	})
 }
